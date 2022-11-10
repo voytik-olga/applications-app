3805ac8f4f379effa1638aed014268e37eca6ca8
czo02 <zobel.christian@gmail.com>
2022-11-10 14:53:26 +0100

    add initial version of app

---
+++ applications/backend/src/main/java/searchrequests/HelloWorldController.java
@@ -0,0 +1,14 @@
+package searchrequests;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @RequestMapping("/")
+    public String hello() {
+        return "Hello World";
+    }
+
+}
---
+++ applications/database/database-readme.md
@@ -0,0 +1,5 @@
+## Database
+
+Just run `docker-compose up` in this directory and the database will be available under localhost:5432.
+
+https://docs.docker.com/compose/
