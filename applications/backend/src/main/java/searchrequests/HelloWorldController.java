4265a00bb02d1bc4aafa14a79a44df35b22b019c
czo02 <zobel.christian@gmail.com>
2022-11-10 18:34:03 +0100

    add lombok

--- applications/backend/src/main/java/searchrequests/HelloWorldController.java
+++ applications/backend/src/main/java/searchrequests/HelloWorldController.java
@@ -1,13 +1,16 @@
 package searchrequests;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class HelloWorldController {
 
     @RequestMapping("/")
     public String hello() {
+        log.info("Hello World was called!");
         return "Hello World";
     }
 
